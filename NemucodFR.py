8d0e953843d8b5d438cd12dc60a6289a95b10fda
Antelox <anteloxrce@gmail.com>
2016-05-22 14:37:29 +0200

    Version 0.2
    - updated the header of each files;
    - NemucodKE.py script now is able to detect encrypted files by 7zip
    variant;
    - NemucodKE.py and NemucodFR.py scripts now handle 2 different Nemucod
    variants (header = 1024 bytes - key = 36 bytes and header = 2048 bytes -
    key = 255 bytes).

--- NemucodFR.py
+++ NemucodFR.py
@@ -5,14 +5,19 @@ Nemucod File Recovery
 Coded by Antelox
 Twitter: @Antelox
 UIC R.E. Academy - quequero.org
+Version: 0.2 - 05/22/2016
 
 Released under MIT License
 
-This script is able to recover encrypted files by Nemucod Ransomware (last variant)
+This script is able to recover encrypted files by Nemucod Ransomware (the last 2 variants)
 
 For more informations about this variant you can take a look at:
 
 https://glot.io/snippets/ee7hiif87k
+
+and here:
+
+http://www.bleepingcomputer.com/forums/t/608045/crypted-ransomware-nemucod-decrypttxt-support-and-help-topic/page-11#entry4005316
 
 DISCLAIMER
 Use this script at your own risk. I'm not responsible for any data loss!
@@ -24,7 +29,7 @@ import sys
 import glob
 import os
 
-def decrypt(file, key):
+def decrypt(file, key, hsize, ksize):
 	try:
 		fencrypted = open(file, 'rb')
 		
@@ -34,18 +39,18 @@ def decrypt(file, key):
 		recovered = ''
 		full_recovered = ''
 		
-		if filesize <= 1024:
+		if filesize <= hsize:
 			for i in range(0,filesize):
-				recovered = recovered + chr(ord(content_encrypted[i])^ord(key[i%36]))
+				recovered = recovered + chr(ord(content_encrypted[i])^ord(key[i%ksize]))
 			full_recovered = recovered
 		else:
-			for i in range(0,1024):
-				recovered = recovered + chr(ord(content_encrypted[i])^ord(key[i%36]))
-			full_recovered = recovered + content_encrypted[1024:]
+			for i in range(0,hsize):
+				recovered = recovered + chr(ord(content_encrypted[i])^ord(key[i%ksize]))
+			full_recovered = recovered + content_encrypted[hsize:]
 		
 		open(file[:-8], 'wb').write(full_recovered)
 		fencrypted.close()
-		print "[-]File %s recovered succesfully!\n" % file[:-8]
+		print "[+]File %s recovered succesfully!\n" % file[:-8]
 		return 0
 	except:
 		print "An error has occured!"
@@ -53,12 +58,14 @@ def decrypt(file, key):
 #main
 print "\n*Nemucod File Recovery*\n"
 
-if len(sys.argv) == 3:
+if len(sys.argv) == 5:
 	os.chdir(sys.argv[1])
 	for file in glob.glob("*.crypted"):
-		decrypt(file,sys.argv[2])
+		decrypt(file,sys.argv[2],sys.argv[3],sys.argv[4])
 else:
 	print "*ERROR: Incorrect number of arguments passed to the script!\n"
-	print "Example: python NemucodFR.py folder key\n"
+	print "Example: python NemucodFR.py folder key header_size key_size\n"
 	print "folder: a folder which contains encrypted files by Nemucod with .crypted extension;\n"
-	print "key: the key recovered with the crack.py script."
+	print "key: the key recovered with the NemucodKE.py script;\n"
+	print "header_size: size, from beginning of the file, which it was encrypted - provided by NemucodKE.py script;\n"
+	print "key_size: size of the key - provided by NemucodKE.py script."
--- NemucodKE.py
+++ NemucodKE.py
@@ -5,13 +5,19 @@ Nemucod Key Extractor
 Coded by Antelox
 Twitter: @Antelox
 UIC R.E. Academy - quequero.org
+Version: 0.2 - 05/22/2016
 
 Released under MIT License
 
-This script is designed to work with the last variant of Nemucod Ransomware. For more informations you can take a look here:
+This script is designed to work with the 2 last variants of Nemucod Ransomware.
+
+For more informations you can take a look here:
 
 https://glot.io/snippets/ee7hiif87k
 
+and here: 
+
+http://www.bleepingcomputer.com/forums/t/608045/crypted-ransomware-nemucod-decrypttxt-support-and-help-topic/page-11#entry4005316
 
 DISCLAIMER
 Use this script at your own risk. I'm not responsible for any data loss!
@@ -27,12 +33,25 @@ if len(sys.argv)== 3:
 	fencrypted = open(sys.argv[1],'rb')
 	fplain = open(sys.argv[2],'rb')
 
-	content_encrypted = fencrypted.read(36)
-	content_plain = fplain.read(36)
-
+	content_encrypted = fencrypted.read()
+	content_plain = fplain.read()
+	
+	if content_encrypted[0]=='7' and content_encrypted[1]=='z':
+		print "*INFO*: 7zip variant detected! This script can not handle it."
+		sys.exit()
+		
+	if content_encrypted[1535] == content_plain[1535] and content_encrypted[2000] == content_plain[2000]:
+		print "*INFO*: encrypted header size = 1024 bytes and key size = 36 bytes\n"
+		print "*INFO*: pass these 2 numbers as argument to NemucodFR.py script.\n"
+		key_size = 36
+	else:
+		print "*INFO*: encrypted header size = 2048 bytes and key size = 255 bytes\n"
+		print "*INFO*: pass these 2 numbers as argument to NemucodFR.py script.\n"
+		key_size = 255
+	
 	key = ''
 
-	for i in range(0,36):
+	for i in range(0,key_size):
 		key = key + chr(ord(content_encrypted[i])^ord(content_plain[i]))
 
 	print "\n*KEY FOUND*: The key is: %s" % key
