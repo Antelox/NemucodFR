ebe513bdf3280c89cd6b9e313914b687e41ecbbb
Michael Gillespie <demonslay335@gmail.com>
2016-05-21 20:56:55 -0500

    Optional Key Length
    Added optional argument for length of Nemucod key; default 36 bytes, 255 bytes seen in wild.

--- NemucodKE.py
+++ NemucodKE.py
@@ -1,38 +1,35 @@
 """
-
 Nemucod Key Extractor
-
 Coded by Antelox
 Twitter: @Antelox
 UIC R.E. Academy - quequero.org
-
 Released under MIT License
-
 This script is designed to work with the last variant of Nemucod Ransomware. For more informations you can take a look here:
-
 https://glot.io/snippets/ee7hiif87k
-
-
 DISCLAIMER
 Use this script at your own risk. I'm not responsible for any data loss!
 Anyway this script print only the key extracted. No file operations will be performed.
-
 """
 
 import sys
 
 print "\n*Nemucod Key Extractor*\n"
 
-if len(sys.argv)== 3:
+if len(sys.argv) == 4:
+	keylen = int(sys.argv[3])
+else:
+	keylen = 36
+
+if len(sys.argv) >= 3:
 	fencrypted = open(sys.argv[1],'rb')
 	fplain = open(sys.argv[2],'rb')
 
-	content_encrypted = fencrypted.read(36)
-	content_plain = fplain.read(36)
+	content_encrypted = fencrypted.read(keylen)
+	content_plain = fplain.read(keylen)
 
 	key = ''
 
-	for i in range(0,36):
+	for i in range(0,keylen):
 		key = key + chr(ord(content_encrypted[i])^ord(content_plain[i]))
 
 	print "\n*KEY FOUND*: The key is: %s" % key
@@ -41,6 +38,6 @@ if len(sys.argv)== 3:
 	fplain.close()
 else:
 	print "*ERROR: Incorrect number of arguments passed to the script!\n"
-	print "Example: python NemucodKE.py encrypted original\n"
+	print "Example: python NemucodKE.py encrypted original [keylen]\n"
 	print "encrypted: a file encrypted by Nemucod with .crypted extension;\n"
 	print "original: the same file not encrypted of which you have a backup copy."
