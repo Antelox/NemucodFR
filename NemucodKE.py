5acdc0e428447cb2a49263646ba3402622e610fc
@Antelox <anteloxrce@gmail.com>
2016-06-11 16:43:44 +0200

    Version 0.3

--- NemucodKE.py
+++ NemucodKE.py
@@ -5,19 +5,20 @@ Nemucod Key Extractor
 Coded by Antelox
 Twitter: @Antelox
 UIC R.E. Academy - quequero.org
-Version: 0.2 - 05/22/2016
+Version: 0.3 - 06/11/2016
 
 Released under MIT License
 
-This script is designed to work with the 2 last variants of Nemucod Ransomware.
+This script is able to recover encrypted files by Nemucod Ransomware (the last variants)
+NOTE: This script doesn't handle 7-Zip variant
 
-For more informations you can take a look here:
+For more information about the last Nemucod variants you can take a look at:
 
-https://glot.io/snippets/ee7hiif87k
+- https://glot.io/snippets/ee7hiif87k
+- http://www.bleepingcomputer.com/forums/t/608045/crypted-ransomware-nemucod-decrypttxt-support-and-help-topic/page-11#entry4005316
+- http://www.bleepingcomputer.com/forums/t/608045/crypted-ransomware-nemucod-decrypttxt-support-and-help-topic/page-12#entry4016916
+- https://reaqta.com/2016/06/nemucod-meets-php/
 
-and here: 
-
-http://www.bleepingcomputer.com/forums/t/608045/crypted-ransomware-nemucod-decrypttxt-support-and-help-topic/page-11#entry4005316
 
 DISCLAIMER
 Use this script at your own risk. I'm not responsible for any data loss!
@@ -29,7 +30,7 @@ import sys
 
 print "\n*Nemucod Key Extractor*\n"
 
-if len(sys.argv)== 3:
+if len(sys.argv) == 3:
 	fencrypted = open(sys.argv[1],'rb')
 	fplain = open(sys.argv[2],'rb')
 
@@ -41,23 +42,53 @@ if len(sys.argv)== 3:
 		sys.exit()
 		
 	if content_encrypted[1535] == content_plain[1535] and content_encrypted[2000] == content_plain[2000]:
-		print "*INFO*: encrypted header size = 1024 bytes and key size = 36 bytes\n"
-		print "*INFO*: pass the first number as argument to NemucodFR.py script.\n"
-		key_size = 36
+		print "*INFO*: encrypted header size = 1024 bytes\n" 
+		print "*INFO*: due to different Nemucod variants key length can be as follow:\n"
+		print "*INFO*: - 36 bytes\n"
+		print "*INFO*: - 1024 bytes\n"
+		print "*INFO*: - 102 bytes\n"
+		print "*INFO*: Run again this script passing as argument the key size.\n"
+		print "*INFO*: For example - python NemucodKE.py encrypted original 102"
 	else:
 		print "*INFO*: encrypted header size = 2048 bytes and key size = 255 bytes\n"
-		print "*INFO*: pass the first number as argument to NemucodFR.py script.\n"
 		key_size = 255
+		
+		key = ''
+		
+		for i in range(0,key_size):
+			if i == key_size - 1:
+				key = key + hex(ord(content_encrypted[i])^ord(content_plain[i]))
+			else:
+				key = key + hex(ord(content_encrypted[i])^ord(content_plain[i])) + ", "
+	
+		print "\n*KEY FOUND*: key.txt file created."
+		open("key.txt", "wb").write("[" + key + "]")
+	
+		fencrypted.close()
+		fplain.close()
+		sys.exit()
+	
+elif len(sys.argv) == 4:
+	fencrypted = open(sys.argv[1],'rb')
+	fplain = open(sys.argv[2],'rb')
+
+	content_encrypted = fencrypted.read()
+	content_plain = fplain.read()
 	
 	key = ''
-
+	key_size = int(sys.argv[3])
 	for i in range(0,key_size):
-		key = key + chr(ord(content_encrypted[i])^ord(content_plain[i]))
+		if i == key_size-1:
+			key = key + hex(ord(content_encrypted[i])^ord(content_plain[i]))
+		else:
+			key = key + hex(ord(content_encrypted[i])^ord(content_plain[i])) + ", "
 
-	print "\n*KEY FOUND*: The key is: %s" % key
+	print "\n*KEY FOUND*: key.txt file created."
+	open("key.txt", "wb").write("[" + key + "]")
 
 	fencrypted.close()
 	fplain.close()
+
 else:
 	print "*ERROR: Incorrect number of arguments passed to the script!\n"
 	print "Example: python NemucodKE.py encrypted original\n"
