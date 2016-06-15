074f18cebe235380fd875db894c0dd303f24320b
@Antelox <anteloxrce@gmail.com>
2016-06-15 17:12:51 +0200

    Version 0.35

--- NemucodFR.py
+++ NemucodFR.py
@@ -5,7 +5,7 @@ Nemucod File Recovery
 Coded by Antelox
 Twitter: @Antelox
 UIC R.E. Academy - quequero.org
-Version: 0.3 - 06/11/2016
+Version: 0.35 - 06/15/2016
 
 Released under MIT License
 
@@ -27,7 +27,6 @@ Anyway the script doesn't overwrite and/or delete any files.
 """
 
 import sys
-import glob
 import os
 import ast
 
@@ -53,7 +52,7 @@ def decrypt(file, key, hsize):
 		
 		open(file[:-8], 'wb').write(full_recovered)
 		fencrypted.close()
-		print "[+] File %s recovered succesfully!\n" % file[:-8]
+		print "[+] File \"%s\" recovered succesfully!\n" % file[:-8]
 		return 0
 	except:
 		print "An error has occured!"
@@ -62,12 +61,14 @@ def decrypt(file, key, hsize):
 print "\n*Nemucod File Recovery*\n"
 
 if len(sys.argv) == 4:
-	os.chdir(sys.argv[1])
-	for file in glob.glob("*.crypted"):
-		decrypt(file, sys.argv[2], int(sys.argv[3]))
+	for subdir, dirs, files in os.walk(str(sys.argv[1])):
+		for file in files:
+			if file.endswith('.crypted'):
+				full_path = os.path.join(subdir, file)
+				decrypt(full_path, sys.argv[2], int(sys.argv[3]))
 else:
 	print "*ERROR: Incorrect number of arguments passed to the script!\n"
-	print "Example: python NemucodFR.py folder key_file header_size\n"
-	print "folder: a folder which contains encrypted files by Nemucod with .crypted extension;\n"
+	print "Example: python NemucodFR.py path key_file header_size\n"
+	print "path: a path t a folder which contains encrypted files by Nemucod with .crypted extension;\n"
 	print "key_file: the file which contains the recovered key by NemucodKE.py script;\n"
 	print "header_size: size, from beginning of the file, which it was encrypted - info provided by NemucodKE.py script."
